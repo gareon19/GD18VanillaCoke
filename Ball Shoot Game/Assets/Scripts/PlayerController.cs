6029a8948a3e1cc22eb59058d561c66441725c72
gareon19 <alain.kolp@hotmail.com>
2018-03-14 04:30:04 +0100

    first commit

---
+++ Ball Shoot Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+        movePlayer();
+	}
+
+    private void movePlayer() {
+
+
+        //Vector3 direction = new Vector3(h, v, 0f).normalized;
+
+        //transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
