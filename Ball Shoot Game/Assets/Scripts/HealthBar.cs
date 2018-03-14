6576faf5f2c3a04ec5c831ea60b45eb8c0102f5d
gareon19 <alain.kolp@hotmail.com>
2018-03-15 00:41:49 +0100

    added bullet, shooting mechanics and health bar

---
+++ Ball Shoot Game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public GameObject player;
+
+    private float health;
+    private float maxHealth;
+    private Vector2 startScale;
+    private Renderer rend;
+
+    void Start() {
+        maxHealth = player.GetComponent<PlayerController>().getHealth();
+        startScale = transform.localScale;
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+	void Update () {
+        if (player != null) {
+            health = player.GetComponent<PlayerController>().getHealth();
+            if (player.tag == "Player 1") {
+                scaleAndPositionBar(1f);
+            } else {
+                scaleAndPositionBar(-1f);
+            }
+        }
+	}
+
+    private void scaleAndPositionBar(float alignmentRL) {
+        float originalValue = rend.bounds.min.x;
+        // scale the health bar
+        transform.localScale = new Vector2(startScale.x * health / maxHealth, startScale.y);
+        float newValue = rend.bounds.min.x;
+        float difference = newValue - originalValue;
+        //move the bar to the alignment (left (1) or right (-1))
+        transform.Translate(new Vector2(-difference*alignmentRL, 0f));
+    }
+}
--- Ball Shoot Game/Assets/Scripts/PlayerController.cs
+++ Ball Shoot Game/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    public float health;
+
     public float speed;
+    public float shootingRate;
+    public GameObject bullet;
+    public GameObject bulletSpawn;
+
+    private float nextBullet;
     
 	void Start () {
-		
+		// :)
 	}
 	
 
 	void Update () {
         movePlayer();
+        shooting();
+        checkHealth();
 	}
+
+    public float getHealth() {
+        return health;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll) {
+        if (coll.gameObject.tag == "Bullet") {
+            Destroy(coll.gameObject);
+            health -= 20;
+        }
+    }
 
     // moves the player
     private void movePlayer() {
@@ -31,5 +51,26 @@ public class PlayerController : MonoBehaviour {
         Vector3 direction = new Vector3(moveHorizontal, moveVertical, 0f).normalized;
         transform.Translate(direction * speed * Time.deltaTime);
     }
-    
+
+    // shoots bullets
+    private void shooting() {
+        if (gameObject.tag == "Player 1" && Input.GetButton("Fire1-Player-1") && Time.time > nextBullet) {
+            shootBullet(1f, 0f);
+        } else if (gameObject.tag == "Player 2" && Input.GetButton("Fire1-Player-2") && Time.time > nextBullet) {
+            shootBullet(-1f, 0f);
+        }
+    }
+
+    private void shootBullet(float directionX, float directionY) {
+        nextBullet = Time.time + shootingRate;
+        GameObject newBullet = Instantiate(bullet, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+        newBullet.GetComponent<Bullet>().setDirection(new Vector2(directionX, directionY));
+    }
+
+    private void checkHealth() {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+
 }
