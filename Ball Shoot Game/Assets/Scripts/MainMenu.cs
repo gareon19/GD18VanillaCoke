7de8a69899075551052f3a8020df5b896df006c0
kevin-meister <kevin.meister@students.unibe.ch>
2018-04-30 18:37:09 +0200

    added main menu and pause menu

---
+++ Ball Shoot Game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        // loads the next scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // does not work in the editor
+        Application.Quit();
+    }
+
+}
---
+++ Ball Shoot Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else {
+                Pause();
+            }
+        }
+	}
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        // pause the running game by setting the timescale to 0
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        // hardcoded for now
+        SceneManager.LoadScene("Menu");
+    }
+}
